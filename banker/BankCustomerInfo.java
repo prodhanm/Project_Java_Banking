e61bcfd16eb9b7412311d374d5585bb13b67aa6c
prodhanm <mustafizur.prodhan@gmail.com>
2023-11-06 10:33:38 -0500

    the customer class is extending from both user and bankaccount via inheritance protocol.

---
+++ banker/BankCustomerInfo.java
@@ -0,0 +1,101 @@
+package banker;
+
+public class BankCustomerInfo extends BankUser{
+    
+    private String name;
+    private String address;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String phoneNumber;
+    private String email;
+    private String bankAccountNumber;
+    
+    public BankCustomerInfo(int id, 
+                            String userName,  
+                            String name, 
+                            String address, 
+                            String city, 
+                            String state, 
+                            String zipCode,
+                            String phoneNumber, 
+                            String email, 
+                            String bankAccountNumber){
+        super(id, userName, bankAccountNumber);
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        ;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }   
+
+    public String getAddress(){
+        return address;
+    }
+
+    public void setAddress(String address){
+        this.address = address;
+    }
+
+    public String getCity(){
+        return city;
+    }
+
+    public void setCity(String city){ 
+        this.city = city;
+    }
+
+    public String getState(){
+        return state;
+    }
+
+    public void setState(String state){
+        this.state = state;
+   }
+
+    public String getZipCode(){
+        return zipCode;
+    }   
+
+    public void setZipCode(String zipCode){
+        this.zipCode = zipCode;
+    }   
+    
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getBankAccountNumber(){
+        return bankAccountNumber;
+    }
+
+    public void setBankAccountNumber(String bankAccountNumber){
+        this.bankAccountNumber = bankAccountNumber;
+    }
+
+}
+    
--- banker/BankerInfo.java
+++ banker/BankerInfo.java
@@ -5,10 +5,29 @@ public class BankerInfo {
         // create an instance of the BankAccountNumber class
         BankAccountNumber accountNumber = new BankAccountNumber();
         BankUser user = new BankUser(1, "John", "123456");
-        String number = accountNumber.getBankAccountNumber();
-        System.out.println("Account number: " + number);
-        System.out.println("User ID: " + user.getId());
+        String email = "john.doe@gmail.com";
+        BankCustomerInfo customer = new BankCustomerInfo(1, 
+                                                         "John",
+                                                         " John Doe",
+                                                         "123 Main St", 
+                                                         "Anytown", 
+                                                         "Anystate", 
+                                                         "12345", 
+                                                         "123-456-7890", 
+                                                         email, 
+                                                         accountNumber.getBankAccountNumber());
+
+        System.out.println("Account number: " + accountNumber.getBankAccountNumber());
+        System.out.println("User ID: " + customer.getId());
         System.out.println("User name: " + user.getUserName()); 
         System.out.println("User password: " + user.getPassword()); 
+        System.out.println("Customer name: " + customer.getName());
+        System.out.println("Customer address: " + customer.getAddress());
+        System.out.println("Customer city: " + customer.getCity());
+        System.out.println("Customer state: " + customer.getState());
+        System.out.println("Customer zip code: " + customer.getZipCode());
+        System.out.println("Customer phone number: " + customer.getPhoneNumber());
+        System.out.println("Customer email: " + customer.getEmail());
+        System.out.println("Customer bank account number: " + customer.getBankAccountNumber());
     }
 }
