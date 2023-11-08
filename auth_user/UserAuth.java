8f4f824ebaf4558509c6efe8559f0c132cb7e01b
prodhanm <mustafizur.prodhan@gmail.com>
2023-11-08 12:23:43 -0500

    set up user authentication.

--- auth_user/UserAuth.java
+++ auth_user/UserAuth.java
@@ -1,19 +1,25 @@
 package auth_user;
 
-public class UserAuth {
-    BankUser bankUser = new BankUser();
-
+public class UserAuth extends BankUserRegister{
     
+    public UserAuth(int id,
+                    String userName, 
+                    String password, 
+                    String firstName, 
+                    String lastName, 
+                    String address, 
+                    String city, 
+                    String state, 
+                    String zipCode, 
+                    String phoneNumber, 
+                    String email) {
+        super(id, userName, password, firstName, lastName, address, city, state, zipCode, phoneNumber, email);
+    }
+
+    public boolean authenticate(String userName, String password) {
+        if (userName.equals(String.valueOf(getId())) && password.equals(getPassword())) {
+            return true;
+        }
+        return false;
+    }
 }
-   
-
-    
-
-/*  
- *  BankUser bankUser = new BankUser(1, "jdoe", "password");
-            System.out.println("User Name: " + bankUser.getUserName());
-            System.out.println("Password: " + bankUser.getPassword());
-
-            String userName = bankUser.getUserName();
-            String password = bankUser.getPassword();
-*/
